ad437b9b1dcdd3ff3663452d298b8780713789a7
Guy Shaked <guy@klinger.consulting>
2025-04-25 12:39:03 +0300

    update

---
+++ src/python_sdk/orm/base/__init__.py
@@ -0,0 +1,1 @@
+from .find_result import *
---
+++ src/python_sdk/orm/base/find_result.py
@@ -0,0 +1,33 @@
+from collections.abc import AsyncIterator, Iterator
+from typing import (
+    Callable,
+    Generic,
+    Type,
+    TypeVar,
+)
+
+from ..models.base_model import BaseModel
+
+T = TypeVar("T", bound=BaseModel)
+
+
+class FindAsyncResult(Generic[T]):
+    def __init__(self, generator_func: Callable[[], AsyncIterator[Type[T]]]):
+        self._generator_func = generator_func
+
+    def __aiter__(self) -> AsyncIterator[Type[T]]:
+        return self._generator_func()
+
+    async def to_list(self) -> list[Type[T]]:
+        return [item async for item in self._generator_func()]
+
+
+class FindResult(Generic[T]):
+    def __init__(self, generator_func: Callable[[], Iterator[Type[T]]]):
+        self._generator_func = generator_func
+
+    def __iter__(self) -> Iterator[Type[T]]:
+        return self._generator_func()
+
+    def to_list(self) -> list[Type[T]]:
+        return [item for item in self._generator_func()]
