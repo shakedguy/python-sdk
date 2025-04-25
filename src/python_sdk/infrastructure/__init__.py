c4dec33c076044f06d23ada086756da524282a8f
Guy Shaked <guy@klinger.consulting>
2025-04-25 22:32:04 +0300

    update

--- src/python_sdk/infrastructure/__init__.py
+++ src/python_sdk/infrastructure/__init__.py
@@ -1,22 +1,48 @@
 import asyncio
 import logging
+import sys
 from typing import Any
 
-from .cache import init_cache
-from .db import MongoClients, PostgresConnectionPool
+from ..conf.logger import get_logger
+
+logger = get_logger("PythoSDK.Infrastructure", level=logging.DEBUG)
+
+
+def init(
+    *,
+    init_postgres: bool = True,
+    init_cache: bool = True,
+    init_mongo: bool = True,
+    init_messaging: bool = True,
+) -> int:  # noqa:
+    logger.debug("Initializing resources")
+
+    if init_postgres:
+        from .db import PostgresConnectionPool
+
+        PostgresConnectionPool.init_pools()
+
+    if init_cache:
+        from .cache import init_cache
+
+        init_cache()
+
+    if init_mongo:
+        init_mongo_clients()
+    logger.debug("Initialization completed successfully")
+
+    return 0
 
 
 def cleanup(*args: Any) -> int:  # noqa:
-    from ..conf.logger import get_logger
     from .cache import RedisClient
     from .db import Mongo, Postgres
     from .messaging import RabbitMQ
 
-    logger = get_logger("PythoSDK.Infrastructure", level=logging.DEBUG)
     logger.debug("Cleaning up resources")
+    Mongo.close()
     RabbitMQ.close()
     Postgres.close()
-    Mongo.close()
     RedisClient.close()
 
     logger.debug("Cleanup completed successfully")
@@ -24,12 +50,10 @@ def cleanup(*args: Any) -> int:  # noqa:
 
 
 async def cleanup_async(*args: Any) -> int:  # noqa:
-    from ..conf.logger import get_logger
     from .cache import RedisClient
     from .db import Mongo, Postgres
     from .messaging import RabbitMQ
 
-    logger = get_logger("PythoSDK.Infrastructure", level=logging.DEBUG)
     logger.debug("Cleaning up resources")
     Mongo.close()
     await asyncio.gather(
@@ -40,18 +64,58 @@ async def cleanup_async(*args: Any) -> int:  # noqa:
     return 0
 
 
-async def init_async(*args: Any) -> int:  # noqa:
-    from ..conf.logger import get_logger
-    from .cache import RedisClient
-    from .db import Mongo, Postgres
-    from .messaging import RabbitMQ
-
-    logger = get_logger("PythoSDK.Infrastructure", level=logging.DEBUG)
+async def init_async(
+    *,
+    init_postgres: bool = True,
+    init_cache: bool = True,
+    init_mongo: bool = True,
+    init_messaging: bool = True,
+) -> int:  # noqa:
     logger.debug("Initializing resources")
-    MongoClients.init_clients()
-    await asyncio.gather(
-        init_cache(),
-    )
+
+    tasks = []
+    if init_postgres:
+        from .db import PostgresConnectionPool
+
+        tasks.append(PostgresConnectionPool.init_pools_async())
+    if init_cache:
+        from .cache import init_cache_async
+
+        tasks.append(init_cache_async())
+    if init_mongo:
+        tasks.append(init_mongo_clients_async())
+
+    try:
+        await asyncio.gather(*tasks)
+    except Exception as e:
+        logger.error(f"Failed to initialize resources: {e}")
+        sys.exit(1)
 
     logger.debug("Initialization completed successfully")
     return 0
+
+
+def init_mongo_clients() -> None:
+    from ..orm import Document, View
+    from .db import Mongo
+
+    Mongo.init_clients()
+    for d in Document.get_all_documents():
+        d.create_indexes()
+
+    for v in View.get_all_views():
+        v.create()
+
+
+async def init_mongo_clients_async():
+    from ..orm import Document, View
+    from .db import Mongo
+
+    Mongo.init_clients()
+
+    await asyncio.gather(
+        *(
+            [d.create_indexes_async() for d in Document.get_all_documents()]
+            + [v.create_async() for v in View.get_all_views()]
+        )
+    )
