6b61cfe841e18cb6b7b15b38b493dec04b486cc4
guysha94 <guy.sha@superplay.co>
2025-12-15 12:51:57 +0200

    'config optional dependencies.'

--- src/python_sdk/infrastructure/messaging/__init__.py
+++ src/python_sdk/infrastructure/messaging/__init__.py
@@ -1,3 +1,6 @@
-from .brokers import *
-from .queues import *
-from .rpc import *
+import importlib.util
+
+if importlib.util.find_spec("faststream") is not None:
+    from .brokers import *
+    from .queues import *
+    from .rpc import *
