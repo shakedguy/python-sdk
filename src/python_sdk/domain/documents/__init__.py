96faea588699dc21ba01dd981621fda14273d797
guysha94 <guy.sha@superplay.co>
2025-12-15 12:48:21 +0200

    'config optional dependencies.'

--- src/python_sdk/domain/documents/__init__.py
+++ src/python_sdk/domain/documents/__init__.py
@@ -1,13 +1,19 @@
-from ._documents import (
-    BaseDocument,
-    DocumentIndex,
-    DocumentIndexType,
-)
-from .mongo import (
-    MongoDocument,
-    MongoDocumentTimeStampedModel,
-    MongoDocumentTimeStampedVersionedModel,
-    MongoDocumentVersionModel,
-    MongoView,
-)
-from .qdrant import QdrantDocument, QdrantTimeStampedDocument
+
+import importlib.util
+
+if importlib.util.find_spec("pymongo") is not None:
+    from ._documents import (
+        BaseDocument,
+        DocumentIndex,
+        DocumentIndexType,
+    )
+    from .mongo import (
+        MongoDocument,
+        MongoDocumentTimeStampedModel,
+        MongoDocumentTimeStampedVersionedModel,
+        MongoDocumentVersionModel,
+        MongoView,
+    )
+
+if importlib.util.find_spec("qdrant_client") is not None:
+    from .qdrant import QdrantDocument, QdrantTimeStampedDocument
